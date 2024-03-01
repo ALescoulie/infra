83abaadaaba5e8cdd967fd57268f679ef1254bd7
Alia Lescoulie <alia.lescoulie@gmail.com>
2024-03-01 10:52:51 -0800

    add nvim debug setup

--- hm/hm_modules/nvim/nvim.nix
+++ hm/hm_modules/nvim/nvim.nix
@@ -1,54 +1,71 @@
 { config, lib, pkgs, ... }:
 
-with lib; {
-  
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-    viAlias = true;
-    vimAlias = true;
-    plugins = with pkgs.vimPlugins; [
-      fzf-vim
-      nerdtree
-      nerdtree-git-plugin
-      rainbow
-      vim-airline
-      vim-easymotion
-      vim-floaterm
-      vim-nix
-      vim-plug
-      vim-sleuth
-      tokyonight-nvim
-      coc-nvim
-      coc-rust-analyzer
-      coc-tsserver
-      coq_nvim
-      editorconfig-nvim
-      goyo-vim
-      limelight-vim
-      nerdcommenter
-      taglist-vim
-      vim-fugitive
-      vim-gitgutter
-      vim-terraform
-      vim-test
-      vimtex
-      awesome-vim-colorschemes
-    ];
+let 
 
-    coc = {
+
+  haskellDapPlugins = with pkgs.haskellPackages; [
+    haskell-dap
+    ghci-dap
+    haskell-debug-adapter
+  ];
+
+  pythonDapPlugins = [];
+
+  languageDebugPlugins = 
+    haskellDapPlugins ++ 
+    pythonDapPlugins;
+in
+  with lib;
+  {
+    programs.neovim = {
       enable = true;
-      settings = builtins.fromJSON (builtins.readFile ./coc-settings.json);
+      defaultEditor = true;
+      viAlias = true;
+      vimAlias = true;
+      plugins = with pkgs.vimPlugins; [
+        fzf-vim
+        nerdtree
+        nerdtree-git-plugin
+        rainbow
+        vim-airline
+        vim-easymotion
+        vim-floaterm
+        vim-nix
+        vim-plug
+        vim-sleuth
+        tokyonight-nvim
+        coc-nvim
+        coc-rust-analyzer
+        coc-tsserver
+        coq_nvim
+        editorconfig-nvim
+        goyo-vim
+        limelight-vim
+        nerdcommenter
+        taglist-vim
+        vim-fugitive
+        vim-gitgutter
+        vim-terraform
+        vim-test
+        vimtex
+        awesome-vim-colorschemes
+        nvim-dap
+        nvim-dap-ui
+      ] ++ languageDebugPlugins;
+
+      coc = {
+        enable = true;
+        settings = builtins.fromJSON (builtins.readFile ./coc-settings.json);
+      };
+
+      extraConfig = ''
+        source ${pkgs.vimPlugins.vim-plug}/plug.vim
+       
+        ${builtins.readFile ./init.nvim}
+        ${builtins.readFile ./ide.nvim}
+      '';
     };
 
-    extraConfig = ''
-      source ${pkgs.vimPlugins.vim-plug}/plug.vim
-     
-      ${builtins.readFile ./init.nvim}
-      ${builtins.readFile ./ide.nvim}
-    '';
-    };
-
-  home.packages = with pkgs; [ nodejs nodePackages.npm fortran-language-server ];
-}
+    home.packages = with pkgs; [ nodejs nodePackages.npm fortran-language-server ];
+  }
 
