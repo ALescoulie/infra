a1396970d5c6d534c927db15887486f3deff4f1b
Alia Lescoulie <alia.lescoulie@gmail.com>
2024-04-01 15:57:07 -0700

    add stargazer to devices

---
+++ hm/stargazer.nix
@@ -0,0 +1,35 @@
+{ config, pkgs, lib, ... }:
+
+{
+  programs.home-manager.enable = true;
+
+  home.username = "alia";
+  home.homeDirectory = "/home/alia";
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowUnfreePredicate = (_: true);
+  home.stateVersion = "23.05"; # Please read the comment before changing.
+
+  imports = [
+    ./hm_modules/alacritty.nix
+    ./hm_modules/direnv.nix
+    ./hm_modules/polybar/polybar.nix
+    ./hm_modules/nvim/nvim.nix
+    ./hm_modules/zsh/zsh.nix
+    ./hm_modules/tmux.nix
+  ];
+
+  home.packages = with pkgs; [
+    arandr
+    anki
+    calibre
+    minecraft
+    discord
+    nitrogen
+    polybar
+    cargo
+    prismlauncher
+    deluge
+    strawberry
+  ];
+
+}
