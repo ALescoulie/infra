71a781671157743e8f8296aa323c3ae7558037e5
Alia Lescoulie <alia.lescoulie@gmail.com>
2025-08-09 02:12:49 -0700

    tinkers with nvim hm file

--- hm/hm_modules/nvim/nvim.nix
+++ hm/hm_modules/nvim/nvim.nix
@@ -7,61 +7,22 @@
     viAlias = true;
     vimAlias = true;
     plugins = with pkgs.vimPlugins; [
-      fzf-vim
-      nerdtree
-      nerdtree-git-plugin
-      rainbow
-      vim-airline
-      vim-easymotion
-      vim-floaterm
-      vim-nix
-      vim-plug
-      vim-sleuth
-      adwaita-nvim
-      coc-nvim
-      coc-rust-analyzer
-      coc-tsserver
-      editorconfig-nvim
-      goyo-vim
-      limelight-vim
-      nerdcommenter
-      taglist-vim
-      vim-fugitive
-      vim-gitgutter
-      vim-terraform
-      vim-test
-      vimtex
-      awesome-vim-colorschemes
-      telescope-nvim
-      image-nvim
-      molten-nvim
-      nvim-treesitter-parsers.markdown
-      nvim-treesitter-parsers.haskell
-      nvim-treesitter-parsers.python
+      nvim-treesitter
     ];
-
-    coc = {
-      enable = true;
-      settings = builtins.fromJSON (builtins.readFile ./coc-settings.json);
-    };
-
-    extraConfig = ''
-      source ${pkgs.vimPlugins.vim-plug}/plug.vim
-     
-      ${builtins.readFile ./init.nvim}
-      ${builtins.readFile ./ide.nvim}
-    '';
-
-    extraLuaPackages = [ pkgs.luajitPackages.magick ];
-    extraPackages = [ pkgs.imagemagick ];
-
-  };
 
   home.packages = with pkgs; [ 
     nodejs
     nodePackages.npm
     fortran-language-server
+    haskellPackages.fast-tags
+    haskellPackages.haskell-language-server
     haskellPackages.hoogle
+    haskellPackages.ghci-dap
+    haskellPackages.haskell-dap
+    haskellPackages.haskell-debug-adapter
+    ghc
+    luajitPackages.luarocks-nix
+    lua
   ];
 }
 
