9ba1c809340f893285edd6015579e3116bd41e2a
Alia Lescoulie <alia.lescoulie@gmail.com>
2026-08-27 17:03:47 -0700

    syncs books across machines

--- hm/hm_modules/syncthing.nix
+++ hm/hm_modules/syncthing.nix
@@ -3,10 +3,20 @@
 {
   services.syncthing = {
     enable = true;
-    
-    devices = {
-      macbook = {
-        id  = "FMHIL4I-RAS3OIZ-ND7ECNB-I5KKJ2S-DC6CE2U-ZLUJGAO-57VU24F-SGCD7A5";
+    settings = {
+      devices = {
+        titan = {
+          id  = "FMHIL4I-RAS3OIZ-ND7ECNB-I5KKJ2S-DC6CE2U-ZLUJGAO-57VU24F-SGCD7A5";
+        };
+        discovery = {
+          id = "HTW73T5-AT5SBMQ-A44VFGQ-BJU4XZH-NAA6VMU-6FVIXEN-RTZWKZV-GKSY7A6";
+        };
+      };
+      folders = {
+        books = {
+          path = "~/Books";
+          devices = [ "titan" "discovery" ];
+        };
       };
     };
   };
